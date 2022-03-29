975ab54ac423d8ef2646539559440e9434986131
The Open Developer <75641810+Sharanya1-lab@users.noreply.github.com>
2022-03-29 15:39:37 +0530

    fibonacci series

---
+++ fibonicci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fibonicci
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the minimum value: ");
+        int m = in.nextInt();
+        System.out.print("Enter the maximum  value: ");
+        int n = in.nextInt();
+        in.close();
+        int a = 0, b = 1;
+        while (a <= n) {
+            if (a >= m)
+                System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
